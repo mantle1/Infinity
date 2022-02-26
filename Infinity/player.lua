2356c7899ad2ba2345b406b22696074ed6549e4c
mantle1 <93952412+mantle1@users.noreply.github.com>
2022-02-26 11:19:05 +0000

    Add files via upload

---
+++ Infinity/player.lua
@@ -0,0 +1,132 @@
+
+Player = {}
+
+function love:load()
+    self.x = 100
+    self.y = 0
+    self.width = 20
+    self.height = 60
+    self.xVel = 0
+    self.yVel = 0
+    self.maxSpeed = 200
+    self.acceleration = 4500
+    self.friction = 3500
+    self.graviity = 1500
+    self.jumpamount = -500
+
+    self.grounded = false
+
+    self.physics = {}
+    self.physics.body = love.physics.newBody(World, self.x, self.y, "dynamic")
+    self.physics.body:setFixedRotation(true)
+    self.physics.shape = love.physics.newRectangle(self.width, self.height)
+    self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
+
+end
+
+
+function love:update(dt)
+    self:syncPhysics(dt)
+    self:moved(dt)
+    self:applyGravity(dt)
+
+end
+
+function Player:applyGravity(dt)
+    if not self.grounded then
+     self.yVel = self.yVel + self.graviity * dt
+    end
+end
+
+function Player:move(dt)
+    if love.keyboard.isDown("d") then 
+        if self.xVel < self.maxSpeed then
+            if self.xVel + self.acceleration * dt < self.maxSpeed then
+            self.xVel = self.xVel + self.acceleration * dt
+            else
+                self.xVel = self.maxSpeed
+            end
+        end
+    elseif love.keyboard.isDown("a") then
+        if self.xVel < self.maxSpeed then
+            if self.xVel + self.acceleration * dt > -self.maxSpeed then
+            self.xVel = self.xVel + self.acceleration * dt
+            else
+                self.xVel = -self.maxSpeed
+            end
+        end
+    else
+        self:applyFriction(dt)
+    end
+end
+
+function Player:applyFriction(dt)
+    if self.xVel > 0 then
+        if self.xVel - self.friction * dt > 0 then
+            self.xVel = self.xVel - self.friction * dt
+        else
+            self.xVel = 0
+        end
+    elseif self.xVel < 0 then
+        if self.xVel + self.friction * dt < 0 then 
+            self.xVel = self.xVel + self.friction * dt
+        else
+            self.xVel = 0
+        end
+    end
+end
+
+    
+
+
+
+
+function Player:syncPhysics()
+    self.x, self.y = self.physics.body:getPostion()
+    self.physics.body:setLinearVelocity(self.xVel, self.yVel)
+end
+
+function Player:beginContact(a, b, collision)
+    if self.grounded == true then return end 
+   local nx, ny = collision:getNormal()
+   if a == self.physics.fixture then
+    if ny > 0 then
+        self:land(collision)
+      end
+    elseif b == self.physics.fixture then
+        if ny < 0 then
+            self:land(collision)
+          end
+
+    end
+end
+
+function Player:land(collision)
+    self.currentGrounded = collision
+    self.yVel = 0
+    self.grounded = true
+end
+
+function Player:jump(key)
+    if (key == "w") and self.grounded then
+        self.yVel = self.jumpamount
+        self.grounded = false
+    end
+end
+
+
+function Player:endContact(a, b, collision)
+    if a == self.physics.fixture or b == self.physics.fixture then
+        if self.currentGroundedCollision == collision then
+            self.grounded = false
+        end
+    end
+end
+
+
+
+function love.draw()
+    love.graphics.rectangle("fill", self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
+
+
+end
